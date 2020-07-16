5513b296b5ad37b822783ac0a6a1f76cdd1a8ae3
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-07-16 00:18:39 -0300

    Fixed travis file

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -3,5 +3,5 @@
 mkdir build && cd build
 flags="-g -O0 --coverage"
 cmake -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_CXX_FLAGS="$flags" -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .. -- -j2
+cmake --build . -- -j2
 ctest -V --output-on-failure
