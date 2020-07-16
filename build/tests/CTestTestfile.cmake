37128bcec1e9e4b8c05d0a2e7bdea3637bf56661
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-07-15 23:40:02 -0300

    Removed build folder from git and add to gitignore

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo/Documents/codes/texugo/tests
-# Build directory: /home/leo/Documents/codes/texugo/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("unit_tests" "/home/leo/Documents/codes/texugo/build/tests/unit_tests")
-set_tests_properties("unit_tests" PROPERTIES  _BACKTRACE_TRIPLES "/home/leo/Documents/codes/texugo/tests/CMakeLists.txt;11;add_test;/home/leo/Documents/codes/texugo/tests/CMakeLists.txt;0;")
