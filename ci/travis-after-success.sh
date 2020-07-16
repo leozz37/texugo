e5b54d8f6061994d958bf5989d7fe640ed594433
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-07-15 22:36:43 -0300

    Fixing travis file

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$IS_COVERAGE_BUILD" == 1 ] && [ "$COVERITY_SCAN_BRANCH" != 1 ]; then
-  coveralls -e deps -e examples --gcov-options '\-lp'
+  coveralls --exclude lib --exclude tests --gcov-options '\-lp'
 fi
--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$COVERITY_SCAN_BRANCH" == 1 ]; then
-  cat /home/travis/build/leokolln/cppaikit/cov-int/scm_log.txt
   exit
 fi
 
