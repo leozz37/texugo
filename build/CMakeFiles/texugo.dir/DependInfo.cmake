69437df9b0ef245fad5460cdd790b5bfa5f26ace
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-07-15 23:36:46 -0300

    Fixed travis file

---
+++ build/CMakeFiles/texugo.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leo/Documents/codes/texugo/src/com/Connection.cpp" "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/src/com/Connection.cpp.o"
+  "/home/leo/Documents/codes/texugo/src/com/Manager.cpp" "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/src/com/Manager.cpp.o"
+  "/home/leo/Documents/codes/texugo/src/config/Settings.cpp" "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/src/config/Settings.cpp.o"
+  "/home/leo/Documents/codes/texugo/src/log/Logger.cpp" "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/src/log/Logger.cpp.o"
+  "/home/leo/Documents/codes/texugo/src/metrics/MetricsExporter.cpp" "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/src/metrics/MetricsExporter.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
