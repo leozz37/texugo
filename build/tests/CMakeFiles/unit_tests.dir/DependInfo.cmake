28b434cc601538925fc09af620ce19c70c224c13
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-07-15 23:30:12 -0300

    Fixed travis file

---
+++ build/tests/CMakeFiles/unit_tests.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leo/Documents/codes/texugo/tests/com/ConnectionTest.cpp" "/home/leo/Documents/codes/texugo/build/tests/CMakeFiles/unit_tests.dir/com/ConnectionTest.cpp.o"
+  "/home/leo/Documents/codes/texugo/tests/main/main.cpp" "/home/leo/Documents/codes/texugo/build/tests/CMakeFiles/unit_tests.dir/main/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/leo/Documents/codes/texugo/build/CMakeFiles/texugo.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- ci/travis-after-success.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$IS_COVERAGE_BUILD" == 1 ] && [ "$COVERITY_SCAN_BRANCH" != 1 ]; then
-  coveralls --exclude lib --exclude tests --gcov-options '\-lp'
-fi
