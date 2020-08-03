c5dcfb65b9ac2df74530fc3fafdf117b211ca84b
leo <leo@artorias.artorias>
2020-08-02 22:49:50 -0300

    Fuck travis scripts

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -4,7 +4,3 @@ mkdir build && cd build
 flags="-g -O0 --coverage"
 cmake -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_CXX_FLAGS="$flags" -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . -- -j2
-cd tests
-cmake ..
-make -j2
-./unit_test
