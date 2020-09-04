add27326215cb82719be10e0a0a3f8d24150714e
leozz37 <leonardoaugusto287@gmail.com>
2020-09-04 10:43:46 -0300

    Fixed Travis script path #2

--- scripts/github-registry.sh
+++ scripts/github-registry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Github Login
-cat ~/GH_TOKEN.txt | docker login docker.pkg.github.com -u "${GITHUB_USER}" --password-stdin "${GITHUB_PASSWORD}"
+docker login docker.pkg.github.com -u "${GITHUB_USER}" --password "${GITHUB_TOKEN}"
 
 cd "$TRAVIS_BUILD_DIR"/docker || exit
 docker-compose build --pull
