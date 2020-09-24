5b571a365e379c18b6b8fb6dc10d415f84d9a6ed
leozz37 <leonardoaugusto287@gmail.com>
2020-09-24 17:43:20 -0300

    Implemented GitHub pages code

---
+++ docs/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Texugo logo" src="./assets/logo.jpg">
+    <HelloWorld msg="Texugo"/>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
