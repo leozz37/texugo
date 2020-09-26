04561fe43ed6b4a04064e0ea4ebad58d306eca0e
leozz37 <leonardoaugusto287@gmail.com>
2020-09-26 12:44:54 -0300

    Refactored injector

--- examples/injector-multiple.py
+++ examples/injector-multiple.py
@@ -5,7 +5,8 @@ host = "127.0.0.1"
 port = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-while(1):
+
+while 1:
     s.sendall(b'Hello, world')
     data = s.recv(1024)
     print('Received', repr(data))
