d4cbb0fc0ffc8bf673b2a9da6ad2a1bcc079bcd1
leozz37 <leonardoaugusto287@gmail.com>
2020-09-26 17:41:26 -0300

    ✨ message parser

--- examples/injector-json.py
+++ examples/injector-json.py
@@ -7,12 +7,12 @@ port = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-message = {
+payload = {
     "destination": [sys.argv[2]],
-    "payload": sys.argv[3]
+    "message": sys.argv[3]
 }
 
-s.sendall(bytes(str(message), 'utf-8'))
+s.sendall(bytes(str(payload), 'utf-8'))
 data = s.recv(1024)
 print('Received:', data.decode('utf-8'))
 
