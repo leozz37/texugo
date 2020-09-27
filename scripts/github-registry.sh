3c1958640d659f2b0f038b9d02af24704100f2d3
Leonardo <leonardoaugusto287@gmail.com>
2020-09-27 14:07:50 -0300

    💚 fixed travis github deploy script

--- scripts/github-registry.sh
+++ scripts/github-registry.sh
@@ -4,5 +4,5 @@
 echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u "${GITHUB_USER}" --password-stdin
 
 docker-compose build --pull
-docker tag docker_texugo docker.pkg.github.com/"${GITHUB_USER}"/texugo/texugo:latest
+docker tag texugo docker.pkg.github.com/"${GITHUB_USER}"/texugo/texugo:latest
 docker push docker.pkg.github.com/"${GITHUB_USER}"/texugo/texugo:latest
