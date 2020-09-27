bbe55681ac912207aed47ed6757d07547b7fc652
leozz37 <leonardoaugusto287@gmail.com>
2020-09-27 12:10:57 -0300

    💚  fixed travis deploy script

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -4,6 +4,5 @@
 docker login --username $"DOCKER_HUB_USER" --password $"DOCKER_HUB_PASS"
 
 # Push Dockerfile to DockerHub
-cd $"TRAVIS_BUILD_DIR"/docker || exit
 docker-compose build --pull
 docker-compose push
