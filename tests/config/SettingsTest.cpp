8497c2bc1892f993f857d48e0587c23f1735b944
leozz37 <leonardoaugusto287@gmail.com>
2020-09-28 04:32:48 -0300

    💚  fixed settings test

--- tests/config/SettingsTest.cpp
+++ tests/config/SettingsTest.cpp
@@ -12,38 +12,47 @@
      Settings *settings;
  };
 
-//TEST_CASE_METHOD(SettingsFixture, "GetMongoPath") {
-//    Settings testSettings(configPath);
-//
-//    const std::string& result = testSettings.getMongoPath();
-//    const std::string expected = settings->getMongoPath();
-//
-//    REQUIRE(result == expected);
-// }
-//
-//TEST_CASE_METHOD(SettingsFixture, "GetLogPath") {
-//    Settings testSettings(configPath);
-//
-//    const std::string& result = testSettings.getLogPath();
-//    const std::string expected = settings->getLogPath();
-//
-//    REQUIRE(result == expected);
-// }
+TEST_CASE_METHOD(SettingsFixture, "GetMongoPath") {
+    Settings testSettings(configPath);
 
-//TEST_CASE_METHOD(SettingsFixture, "GetMetricsAddress") {
-//    Settings testSettings(configPath);
-//
-//    const std::string& result = testSettings.getMetricsAddress();
-//    const std::string expected = settings->getMetricsAddress();
-//
-//    REQUIRE(result == expected);
-// }
-//
-//TEST_CASE_METHOD(SettingsFixture, "GetRoutingAddresses") {
-//    Settings testSettings(configPath);
-//
-//    const auto& result = testSettings.getReceiverAddresses();
-//    const auto expected = settings->getReceiverAddresses();
-//
-//    REQUIRE(result == expected);
-// }
+    const std::string& result = testSettings.getMongoPath();
+    const std::string expected = settings->getMongoPath();
+
+    REQUIRE(result == expected);
+ }
+
+TEST_CASE_METHOD(SettingsFixture, "GetLogPath") {
+    Settings testSettings(configPath);
+
+    const std::string& result = testSettings.getLogPath();
+    const std::string expected = settings->getLogPath();
+
+    REQUIRE(result == expected);
+ }
+
+TEST_CASE_METHOD(SettingsFixture, "GetMetricsAddress") {
+    Settings testSettings(configPath);
+
+    const std::string& result = testSettings.getMetricsAddress();
+    const std::string expected = settings->getMetricsAddress();
+
+    REQUIRE(result == expected);
+ }
+
+TEST_CASE_METHOD(SettingsFixture, "GetReceiverAddresses") {
+    Settings testSettings(configPath);
+
+    const auto& result = testSettings.getReceiverAddresses();
+    const auto expected = settings->getReceiverAddresses();
+
+    REQUIRE(result == expected);
+ }
+
+TEST_CASE_METHOD(SettingsFixture, "GetSenderAddresses") {
+    Settings testSettings(configPath);
+
+    const auto& result = testSettings.getSenderAddresses();
+    const auto expected = settings->getSenderAddresses();
+
+    REQUIRE(result == expected);
+}
