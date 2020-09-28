a42deceb46993ae16b9efd75df1f539832bf3bee
leozz37 <leonardoaugusto287@gmail.com>
2020-09-28 02:23:23 -0300

    🔥  removed DockerHub deploy script

--- scripts/travis-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# login DockerHub
-docker login --username $"DOCKER_HUB_USER" --password $"DOCKER_HUB_PASS"
-
-# Push Dockerfile to DockerHub
-docker-compose build --pull
-docker-compose push
